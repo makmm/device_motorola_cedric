f85574f1c83b54ff87ce900fe9436a93eb1c0f0e
v0idifier <duckifier@protonmail.ch>
2018-02-17 20:27:04 +0000

    Enable Camera2API

--- device.mk
+++ device.mk
@@ -385,3 +385,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8996/kernel-headers
+
+# Enable Camera2API
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=1
